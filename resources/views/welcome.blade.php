42c44e44deddd975efbb0bd3e23dc31bc7b5dd37
Ryan W. O'Hara <ryan@ryanwohara.com>
2015-03-05 22:09:01 -0500

    Initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>RS Help</title>
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		<link href='/css/app.css' rel='stylesheet' type='text/less'>
+	</head>
+	<body>
+		<div class="container content">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 padding">
+					<div class="row>
+						<div class="col-md-4 col-md-offset-4">
+							<h1>#RSHelp</h1>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-md-offset-3">
+					<div class="row">
+						<div class="col-md-4 col-md-offset-4">
+							<h2><a href="http://swiftirc.net">SwiftIRC.net</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
